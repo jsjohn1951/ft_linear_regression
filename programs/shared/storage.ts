d1cdb7a89976f487fb71ab48ba2e57a995ba61ca
jsjohn1951 <jsjohn1951@users.noreply.github.com>
2026-02-23 14:06:50 +0000

    create directory models to save generated model files

--- programs/shared/storage.ts
+++ programs/shared/storage.ts
@@ -1,4 +1,5 @@
-import { readFileSync, writeFile } from "fs";
+import { existsSync, readFileSync, writeFile } from "fs";
+import { mkdir } from "fs/promises";
 import { DataModel, DataStorage } from "./schema.ts";
 
 export let store: DataStorage = new DataStorage();
@@ -9,10 +10,11 @@ export let model: DataModel = new DataModel();
  * saveModel
  * @description Saves the newly generated theta0, and theta1 as our model
  */
-export function saveModel(
+export async function saveModel(
   modelName: string = "model.json",
   dir: string = "models",
 ) {
+  await mkdir(`./${dir}`, { recursive: true });
   writeFile(`${dir}/${modelName}`, JSON.stringify(model), (err) => {
     if (err) throw err;
     console.log("The file has been saved!");
@@ -24,7 +26,11 @@ export function saveModel(
  * readData
  * @description reads from the data.csv file and stores info within the exported store
  */
-export function readData(file: string = "data.csv", type: 'csv' | 'json' = 'csv') {
+export function readData(
+  file: string = "data.csv",
+  type: "csv" | "json" = "csv",
+) {
+  if (!existsSync(file)) throw new Error("File Not Found!");
   const rawData: string = readFileSync(file, "utf8");
   if (type === "json") {
     const data: DataModel = JSON.parse(rawData);
